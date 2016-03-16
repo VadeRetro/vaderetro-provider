9b89431191d5f71353d66355900739d0d71bf825
ahonore <antoine.honore@vade-retro.com>
2016-03-16 15:27:20 +0100

    fixed: vrkeystore ref

--- src/main/java/com/vaderetrosecure/VadeRetroProvider.java
+++ src/main/java/com/vaderetrosecure/VadeRetroProvider.java
@@ -7,6 +7,7 @@ import java.security.AccessController;
 import java.security.PrivilegedAction;
 import java.security.Provider;
 
+import com.vaderetrosecure.keystore.VRKeystoreSpi;
 import com.vaderetrosecure.ssl.VRKeyManagerFactorySpi;
 import com.vaderetrosecure.ssl.VRSSLContextSpi;
 
@@ -17,27 +18,22 @@ import com.vaderetrosecure.ssl.VRSSLContextSpi;
 public class VadeRetroProvider extends Provider
 {
     private static final long serialVersionUID = -5763788919498367657L;
-
-//    private static final Logger LOG = Logger.getLogger(FileKeystoreProvider.class);
     private static final String VR_PROVIDER = "VR";
 
     public VadeRetroProvider()
     {
         super(VR_PROVIDER, 0.1, "Vade Retro Security provider");
-        
 
         AccessController.doPrivileged(new PrivilegedAction<Object>()
         {
-
             @Override
             public Object run()
             {
-                put("KeyStore.VR", FileKeystore.class.getName());
+                put("KeyStore.VR", VRKeystoreSpi.class.getName());
                 put("SSLContext.TLS", VRSSLContextSpi.class.getName());
                 put("KeyManagerFactory.VR", VRKeyManagerFactorySpi.class.getName());
                 return null;
             }
         });
-
     }
 }
