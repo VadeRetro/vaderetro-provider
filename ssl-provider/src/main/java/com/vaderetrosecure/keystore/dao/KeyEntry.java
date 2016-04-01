b444358a64ec02392c93b02bfd6141aaac95aa22
ahonore <antoine.honore@vade-retro.com>
2016-04-01 18:46:49 +0200

    super class for all key entries

--- ssl-provider/src/main/java/com/vaderetrosecure/keystore/dao/KeyEntry.java
+++ ssl-provider/src/main/java/com/vaderetrosecure/keystore/dao/KeyEntry.java
@@ -8,7 +8,6 @@ import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
-import java.time.Instant;
 import java.util.Date;
 
 import javax.crypto.BadPaddingException;
@@ -19,54 +18,26 @@ import javax.crypto.NoSuchPaddingException;
  * @author ahonore
  *
  */
-public abstract class KeyEntry
+public class KeyEntry extends KeyStoreEntry
 {
-    private String alias;
-    private Date creationDate;
     private String algorithm;
     private byte[] cipheredKey;
     private LockedKeyProtection lockedKeyProtection;
 
     public KeyEntry()
     {
-        this("", Date.from(Instant.now()), "", new byte[]{}, null);
+        super();
+        this.algorithm = null;
+        this.cipheredKey = new byte[]{};
+        this.lockedKeyProtection = null;
     }
 
     public KeyEntry(String alias, Date creationDate, String algorithm, byte[] cipheredKey, LockedKeyProtection lockedKeyProtection)
     {
-        this.alias = alias;
-        this.creationDate = creationDate;
+        super(alias, creationDate);
         this.algorithm = algorithm;
         this.cipheredKey = cipheredKey;
         this.lockedKeyProtection = lockedKeyProtection;
-    }
-
-//    public KeyEntry(String alias, Date creationDate, Key key, char[] password, byte[] salt, byte[] iv) throws InvalidKeyException, NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException
-//    {
-//        this.alias = alias;
-//        this.creationDate = creationDate;
-//        this.algorithm = key.getAlgorithm();
-//        setKey(key, password, salt, iv);
-//    }
-
-    public String getAlias()
-    {
-        return alias;
-    }
-
-    public void setAlias(String alias)
-    {
-        this.alias = alias;
-    }
-
-    public Date getCreationDate()
-    {
-        return creationDate;
-    }
-
-    public void setCreationDate(Date creationDate)
-    {
-        this.creationDate = creationDate;
     }
 
     public String getAlgorithm()
@@ -99,7 +70,12 @@ public abstract class KeyEntry
         this.lockedKeyProtection = lockedKeyProtection;
     }
 
-    public abstract Key getKey(KeyProtection keyProtection) throws InvalidKeyException, NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException;
+    public Key getKey(KeyProtection keyProtection) throws InvalidKeyException, NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException
+    {
+        return null;
+    }
 
-    public abstract void setKey(Key key, KeyProtection keyProtection) throws InvalidKeyException, NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException;
+    public void setKey(Key key, KeyProtection keyProtection) throws InvalidKeyException, NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException
+    {
+    }
 }
