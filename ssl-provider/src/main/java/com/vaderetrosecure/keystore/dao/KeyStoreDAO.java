7f54ada468f1857fae8a847e6e2d6b1c12e5e0d7
ahonore <antoine.honore@vade-retro.com>
2016-04-01 18:49:12 +0200

    modified to use KeyEntry and certificatesEntry objects

--- ssl-provider/src/main/java/com/vaderetrosecure/keystore/dao/KeyStoreDAO.java
+++ ssl-provider/src/main/java/com/vaderetrosecure/keystore/dao/KeyStoreDAO.java
@@ -19,10 +19,22 @@ public interface KeyStoreDAO
      */
     void checkDAOStructure() throws KeyStoreDAOException;
     
-    IntegrityData getIntegrityData() throws KeyStoreDAOException;
-    
+    /**
+     * Return the number of stored entries.
+     * This is the sum of all key and certificate entries.
+     * 
+     * @return an integer representing the number of entries.
+     * @throws KeyStoreDAOException
+     */
     int countEntries() throws KeyStoreDAOException;
     
+    /**
+     * Return the list of all distinct aliases.
+     * This is a combination of aliases from key and certificate entries.
+     * 
+     * @return the list of aliases.
+     * @throws KeyStoreDAOException
+     */
     List<String> getAliases() throws KeyStoreDAOException;
     
     /**
@@ -34,38 +46,37 @@ public interface KeyStoreDAO
      * @throws KeyStoreDAOException
      */
     List<String> getAuthenticationAliases(String keyType) throws KeyStoreDAOException;
-    
-    KeyEntry getKeyEntry(String alias) throws KeyStoreDAOException;
-    
-    CertificateEntry getCertificate(String name) throws KeyStoreDAOException;
-    
-    /**
-     * Return the certificate chain given by the name of the first certificate.
-     * 
-     * @param name the alias or one of the names of the certificate.
-     * @return
-     * @throws KeyStoreDAOException
-     */
-    List<CertificateEntry> getCertificateChain(String name) throws KeyStoreDAOException;
-    
-    /**
-     * Return the date of the creation of the entry in the structure.
-     * 
-     * @param alias
-     * @return
-     * @throws KeyStoreDAOException
-     */
-    DateEntry getDateEntry(String alias) throws KeyStoreDAOException;
-    
-    void setDateEntry(String alias) throws KeyStoreDAOException;
+
+    IntegrityData getIntegrityData() throws KeyStoreDAOException;
     
     void setIntegrityData(IntegrityData integrityData) throws KeyStoreDAOException;
     
-    void setEntry(KeyEntry keyEntry, List<CertificateEntry> certificateEntries) throws KeyStoreDAOException;
+    KeyEntry getKeyEntry(String alias) throws KeyStoreDAOException;
 
-    void setCertificateEntry(CertificateEntry certificateEntry) throws KeyStoreDAOException;
+    void setEntry(KeyEntry keyEntry) throws KeyStoreDAOException;
 
     void deleteKeyEntry(String alias) throws KeyStoreDAOException;
 
-    void deleteCertificateEntry(String alias) throws KeyStoreDAOException;
+    CertificatesEntry getCertificatesEntry(String alias) throws KeyStoreDAOException;
+
+    List<CertificatesEntry> getCertificatesEntries(String name) throws KeyStoreDAOException;
+    
+    void setEntry(CertificatesEntry certificatesEntry) throws KeyStoreDAOException;
+
+    void deleteCertificatesEntry(String alias) throws KeyStoreDAOException;
+
+//    /**
+//     * Return the certificate chain given by the name of the first certificate.
+//     * 
+//     * @param name the alias or one of the names of the certificate.
+//     * @return
+//     * @throws KeyStoreDAOException
+//     */
+//    List<CertificateEntry> getCertificateChain(String name) throws KeyStoreDAOException;
+//    
+//    void setEntry(KeyEntry keyEntry, List<CertificateEntry> certificateEntries) throws KeyStoreDAOException;
+//
+//    void setCertificateEntry(CertificateEntry certificateEntry) throws KeyStoreDAOException;
+//
+//    void deleteCertificateEntry(String alias) throws KeyStoreDAOException;
 }
