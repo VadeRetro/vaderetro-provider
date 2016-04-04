9a9e1104d32ee426582511ab6b13374d8c1dc8bf
ahonore <antoine.honore@vade-retro.com>
2016-04-04 17:45:13 +0200

    - adapted unit tests - added ut with private key and certificate

--- ssl-provider/src/test/java/com/vaderetrosecure/keystore/VRKeyStoreSpiTest.java
+++ ssl-provider/src/test/java/com/vaderetrosecure/keystore/VRKeyStoreSpiTest.java
@@ -3,44 +3,68 @@
  */
 package com.vaderetrosecure.keystore;
 
-import static org.mockito.Matchers.anyCollectionOf;
-import static org.mockito.Matchers.eq;
-import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
-import java.security.GeneralSecurityException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
 import java.security.Key;
+import java.security.KeyFactory;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
+import java.security.PrivateKey;
+import java.security.SecureRandom;
 import java.security.Security;
 import java.security.UnrecoverableKeyException;
+import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
-import java.util.Collection;
+import java.security.cert.CertificateFactory;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.time.Instant;
 import java.util.Collections;
+import java.util.Date;
 import java.util.Properties;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
+import javax.naming.InvalidNameException;
 
+import org.apache.log4j.Logger;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Ignore;
+import org.junit.BeforeClass;
 import org.junit.Test;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.stubbing.Answer;
+import org.mockito.ArgumentCaptor;
 
 import com.vaderetrosecure.VadeRetroProvider;
-import com.vaderetrosecure.keystore.dao.KeyEntry;
-import com.vaderetrosecure.keystore.dao.KeyEntryType;
+import com.vaderetrosecure.keystore.dao.CertificateData;
+import com.vaderetrosecure.keystore.dao.CertificatesEntry;
 import com.vaderetrosecure.keystore.dao.IntegrityData;
+import com.vaderetrosecure.keystore.dao.KeyEntry;
+import com.vaderetrosecure.keystore.dao.KeyProtection;
 import com.vaderetrosecure.keystore.dao.KeyStoreDAO;
 import com.vaderetrosecure.keystore.dao.KeyStoreDAOException;
 import com.vaderetrosecure.keystore.dao.KeyStoreDAOFactory;
+import com.vaderetrosecure.keystore.dao.PrivateKeyEntry;
 
 /**
  * @author ahonore
@@ -49,19 +73,48 @@ import com.vaderetrosecure.keystore.dao.KeyStoreDAOFactory;
 public class VRKeyStoreSpiTest
 {
     private static final String MASTER_PASSWORD = "master-password";
+    private static final String SECRET_KEY_ALIAS = "secret-alias";
+    private static final String PRIVATE_KEY_AND_CERTIFICATE_ALIAS = "private-certificate-alias";
+    private final static Logger LOG = Logger.getLogger(VRKeyStoreSpiTest.class);
 
-    private SecretKey secretKey;
+    private static SecretKey secretKey;
+    private static PrivateKey privKey;
+    private static Certificate cert;
     private VRKeyStoreSpi keystore;
     private KeyStoreDAO ksdao;
+
+    @BeforeClass
+    public static void setUpBeforeClass() throws Exception
+    {
+        secretKey = new SecretKeySpec("secret key".getBytes(StandardCharsets.US_ASCII), "AES");
+
+        try
+        {
+        URL url = Thread.currentThread().getContextClassLoader().getResource("test.com.key");
+        byte[] encData = Files.readAllBytes(Paths.get(url.toURI()));
+        KeyFactory kf = KeyFactory.getInstance("RSA");
+        privKey = kf.generatePrivate(new PKCS8EncodedKeySpec(encData));
+        }
+        catch (Exception e)
+        {
+            LOG.fatal(e, e);
+            throw e;
+        }
+        
+        try (InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream("test.com.crt"))
+        {
+            CertificateFactory cf = CertificateFactory.getInstance("X.509");
+            cert = cf.generateCertificate(is);
+        }
+    }
     
     @Before
     public void setUp() throws Exception
     {
-        secretKey = new SecretKeySpec("secret key".getBytes(StandardCharsets.US_ASCII), "AES");
         
-        IntegrityData ksemd = IntegrityData.generate(MASTER_PASSWORD.toCharArray());
+        IntegrityData id = new IntegrityData(MASTER_PASSWORD.toCharArray());
         ksdao = mock(KeyStoreDAO.class);
-        when(ksdao.getMetaData()).thenReturn(ksemd);
+        when(ksdao.getIntegrityData()).thenReturn(id);
         
         keystore = new VRKeyStoreSpi(ksdao);
     }
@@ -77,77 +130,83 @@ public class VRKeyStoreSpiTest
     }
 
     @Test(expected=IOException.class)
-    public void testLoadWrongPassword() throws IOException, KeyStoreDAOException, UnrecoverableKeyException, GeneralSecurityException
+    public void testLoadWrongPassword() throws NoSuchAlgorithmException, CertificateException, IOException
     {
-        when(ksdao.getMetaData()).thenThrow(new KeyStoreDAOException("")).thenReturn(IntegrityData.generate(MASTER_PASSWORD.toCharArray()));
         keystore.engineLoad(null, MASTER_PASSWORD.toCharArray());
         keystore.engineLoad(null, "sfkghshiistgohstio".toCharArray());
     }
 
     @Test
-    public void testLoadRightPassword() throws KeyStoreException, NoSuchProviderException, NoSuchAlgorithmException, CertificateException, IOException
+    public void testLoadRightPassword() throws NoSuchAlgorithmException, CertificateException, IOException
     {
         keystore.engineLoad(null, MASTER_PASSWORD.toCharArray());
     }
 
-    @Ignore
+    @Test
+    public void testEngineSetKeyEntryWithSecretKey() throws NoSuchAlgorithmException, CertificateException, IOException, KeyStoreException, KeyStoreDAOException, InvalidKeyException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, InvalidKeySpecException, InvalidAlgorithmParameterException
+    {
+        ArgumentCaptor<KeyEntry> argument = ArgumentCaptor.forClass(KeyEntry.class);
+        
+        keystore.engineLoad(null, null);
+        keystore.engineSetKeyEntry(SECRET_KEY_ALIAS, secretKey, null, null);
+        
+        verify(ksdao, never()).setEntry(any(CertificatesEntry.class));
+        verify(ksdao).setEntry(argument.capture());
+        Assert.assertEquals(SECRET_KEY_ALIAS, argument.getValue().getAlias());
+        KeyProtection kp = new KeyProtection(argument.getValue().getLockedKeyProtection(), null);
+        Assert.assertArrayEquals(secretKey.getEncoded(), argument.getValue().getKey(kp).getEncoded());
+    }
+
+    @Test
+    public void testEngineSetEntryWithPrivateKeyAndCertificateChain() throws NoSuchAlgorithmException, CertificateException, IOException, KeyStoreException, KeyStoreDAOException, InvalidKeyException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, InvalidKeySpecException, InvalidAlgorithmParameterException
+    {
+        ArgumentCaptor<KeyEntry> argPKEntry = ArgumentCaptor.forClass(KeyEntry.class);
+        ArgumentCaptor<CertificatesEntry> argCertsEntry = ArgumentCaptor.forClass(CertificatesEntry.class);
+        
+        keystore.engineLoad(null, null);
+        keystore.engineSetKeyEntry(PRIVATE_KEY_AND_CERTIFICATE_ALIAS, privKey, null, new Certificate[]{ cert });
+        
+        verify(ksdao).setEntry(argPKEntry.capture());
+        verify(ksdao).setEntry(argCertsEntry.capture());
+        
+        Assert.assertEquals(PRIVATE_KEY_AND_CERTIFICATE_ALIAS, argPKEntry.getValue().getAlias());
+        KeyProtection kp = new KeyProtection(argPKEntry.getValue().getLockedKeyProtection(), null);
+        Assert.assertArrayEquals(privKey.getEncoded(), argPKEntry.getValue().getKey(kp).getEncoded());
+
+        Assert.assertEquals(PRIVATE_KEY_AND_CERTIFICATE_ALIAS, argCertsEntry.getValue().getAlias());
+        Assert.assertEquals(1, argCertsEntry.getValue().getCertificates().size());
+        Assert.assertArrayEquals(cert.getEncoded(), argCertsEntry.getValue().getCertificates().get(0).getCertificate().getEncoded());
+    }
+
     @Test(expected=UnrecoverableKeyException.class)
-    public void testStoreLoadSecretKeyWrongPassword() throws NoSuchAlgorithmException, CertificateException, IOException, KeyStoreException, UnrecoverableKeyException, KeyStoreDAOException
+    public void testEngineGetKeyEntryWithPrivateKey() throws InvalidKeyException, NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException, KeyStoreDAOException, CertificateException, IOException, UnrecoverableKeyException
     {
-        doAnswer(new Answer<Object>() {
-            @SuppressWarnings("unchecked")
-            public Object answer(InvocationOnMock invocation) throws KeyStoreDAOException {
-                Object[] args = invocation.getArguments();
-                KeyEntry kse = ((Collection<KeyEntry>) args[0]).toArray(new KeyEntry[]{})[0];
-                KeyStoreDAO mock = (KeyStoreDAO) invocation.getMock();
-                when(mock.getKeyEntry(eq("key-alias"), eq(KeyEntryType.SECRET_KEY))).thenReturn(Collections.singletonList(kse));
-                return null;
-            }})
-        .when(ksdao).setKeyEntries(anyCollectionOf(KeyEntry.class));
+        SecureRandom sr = new SecureRandom();
+        byte[] iv = new byte[16];
+        sr.nextBytes(iv);
+        KeyProtection kp = new KeyProtection(PRIVATE_KEY_AND_CERTIFICATE_ALIAS, iv, getAESSecretKey("password".toCharArray(), ksdao.getIntegrityData().getSalt()));
+        PrivateKeyEntry ske = new PrivateKeyEntry(PRIVATE_KEY_AND_CERTIFICATE_ALIAS, Date.from(Instant.now()), privKey, kp);
+        when(ksdao.getKeyEntry(anyString())).thenReturn(ske);
         
-        keystore.engineLoad(null, MASTER_PASSWORD.toCharArray());
-        keystore.engineSetKeyEntry("key-alias", secretKey, "key-password".toCharArray(), null);
-        keystore.engineGetKey("key-alias", "sxfthdfhdhy".toCharArray());
+        keystore.engineLoad(null, null);
+        Key k = keystore.engineGetKey(PRIVATE_KEY_AND_CERTIFICATE_ALIAS, "password".toCharArray());
+        Assert.assertTrue(PrivateKeyEntry.class.isInstance(k));
+        Assert.assertArrayEquals(privKey.getEncoded(), k.getEncoded());
+        
+        keystore.engineGetKey(PRIVATE_KEY_AND_CERTIFICATE_ALIAS, "wfgbkljnsmkgklmjb".toCharArray());
     }
 
     @Test
-    public void testStoreLoadSecretKeyRightPassword() throws NoSuchAlgorithmException, CertificateException, IOException, KeyStoreException, UnrecoverableKeyException, KeyStoreDAOException
+    public void testEngineGetCertificateEntry() throws InvalidNameException, KeyStoreDAOException, NoSuchAlgorithmException, CertificateException, IOException
     {
-        doAnswer(new Answer<Object>() {
-            @SuppressWarnings("unchecked")
-            public Object answer(InvocationOnMock invocation) throws KeyStoreDAOException {
-                Object[] args = invocation.getArguments();
-                KeyEntry kse = ((Collection<KeyEntry>) args[0]).toArray(new KeyEntry[]{})[0];
-                KeyStoreDAO mock = (KeyStoreDAO) invocation.getMock();
-                when(mock.getKeyEntry(eq("key-alias"), eq(KeyEntryType.SECRET_KEY))).thenReturn(Collections.singletonList(kse));
-                return null;
-            }})
-        .when(ksdao).setKeyEntries(anyCollectionOf(KeyEntry.class));
-        keystore.engineLoad(null, MASTER_PASSWORD.toCharArray());
-        keystore.engineSetKeyEntry("key-alias", secretKey, "key-password".toCharArray(), null);
-        Key k = keystore.engineGetKey("key-alias", "key-password".toCharArray());
+        CertificateData cd = new CertificateData(cert);
+        CertificatesEntry ce = new CertificatesEntry(PRIVATE_KEY_AND_CERTIFICATE_ALIAS, Date.from(Instant.now()), Collections.singletonList(cd));
+        when(ksdao.getCertificatesEntry(anyString())).thenReturn(ce);
         
-        Assert.assertArrayEquals(secretKey.getEncoded(), k.getEncoded());
-    }
+        keystore.engineLoad(null, null);
+        Certificate c = keystore.engineGetCertificate(PRIVATE_KEY_AND_CERTIFICATE_ALIAS);
 
-    @Test
-    public void testStoreLoadCertificate() throws NoSuchAlgorithmException, CertificateException, IOException, KeyStoreException, UnrecoverableKeyException, KeyStoreDAOException
-    {
-        doAnswer(new Answer<Object>() {
-            @SuppressWarnings("unchecked")
-            public Object answer(InvocationOnMock invocation) throws KeyStoreDAOException {
-                Object[] args = invocation.getArguments();
-                KeyEntry kse = ((Collection<KeyEntry>) args[0]).toArray(new KeyEntry[]{})[0];
-                KeyStoreDAO mock = (KeyStoreDAO) invocation.getMock();
-                when(mock.getKeyEntry(eq("key-alias"), eq(KeyEntryType.SECRET_KEY))).thenReturn(Collections.singletonList(kse));
-                return null;
-            }})
-        .when(ksdao).setKeyEntries(anyCollectionOf(KeyEntry.class));
-        keystore.engineLoad(null, MASTER_PASSWORD.toCharArray());
-        keystore.engineSetKeyEntry("key-alias", secretKey, "key-password".toCharArray(), null);
-        Key k = keystore.engineGetKey("key-alias", "key-password".toCharArray());
-        
-        Assert.assertArrayEquals(secretKey.getEncoded(), k.getEncoded());
+        Assert.assertArrayEquals(cert.getEncoded(), c.getEncoded());
     }
     
     public static class MockVRKeyStoreDAOFactory extends KeyStoreDAOFactory
@@ -162,5 +221,13 @@ public class VRKeyStoreSpiTest
         {
             return null;
         }
+    }
+    
+    private static SecretKey getAESSecretKey(char[] password, byte[] salt) throws NoSuchAlgorithmException, InvalidKeySpecException
+    {
+        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+        KeySpec spec = new PBEKeySpec(password, salt, 65536, 256);
+        SecretKey tmp = factory.generateSecret(spec);
+        return new SecretKeySpec(tmp.getEncoded(), "AES");
     }
 }
