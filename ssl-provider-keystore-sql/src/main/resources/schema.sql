c900f1fc2f5ca8887fc4a9e515f50b00635a52d4
ahonore <antoine.honore@vade-retro.com>
2016-04-05 18:45:57 +0200

    look up sql schema

--- ssl-provider-keystore-sql/src/main/resources/schema.sql
+++ ssl-provider-keystore-sql/src/main/resources/schema.sql
@@ -1,4 +1,47 @@
-drop table if exists keystore_entries, keystore_metadata, keystore_names;
-create table keystore_metadata (major_version int not null, version varchar(64) not null, salt varchar(128) not null, iv varchar(256) not null, key_iv varchar(256) not null, key_iv_hash varchar(256) not null, primary key (major_version));
-create table keystore_entries (alias_hash varchar(64) not null, alias varchar(256) not null, entry_type int default 0, rank int default 0, creation_date bigint default 0, algorithm varchar(256), data text not null, primary key (alias_hash, entry_type, rank), key (entry_type));
+create table keystore_metadata
+(
+	major_version int not null, 
+	version varchar(64) not null, 
+	salt varchar(128) not null, 
+	iv varchar(256) not null, 
+	key_iv varchar(256) not null, 
+	key_iv_hash varchar(256) not null, 
+	primary key (major_version)
+);
+
+create table keystore_entries
+(
+	alias_hash varchar(64) not null, alias varchar(256) not null, entry_type int default 0, rank int default 0, creation_date bigint default 0, algorithm varchar(256), data text not null, primary key (alias_hash, entry_type, rank), key (entry_type)
+);
+
 create table keystore_names (alias_hash varchar(64) not null, rank int default 0, name_hash varchar(64) not null, name varchar(256) not null, primary key (alias_hash, rank, name_hash), key (name_hash));
+
+create table versions
+(
+	table_name varchar(128) not null,
+	version int not null,
+	primary key(table_name)
+);
+
+create table integrity
+(
+	id bigint not null,
+	salt varchar(128) not null, 
+	iv varchar(128) not null, 
+	data varchar(256) not null, 
+	data_hash varchar(64) not null, 
+	primary key(id)
+);
+
+create table keys
+(
+	alias_hash varchar(64) not null,
+	alias varchar(256) not null,
+	creation_date bigint default 0,
+	key_entry_type int default 0,
+	algorithm varchar(256),
+	data text not null,
+	protection_iv varchar(256),
+	protection_key text,
+	primary key(alias_hash)
+);
