b400c97defa7fbaeaef86646e0d228ac66641075
ahonore <antoine.honore@vade-retro.com>
2016-04-05 18:47:08 +0200

    added sql object mapping

---
+++ ssl-provider-keystore-sql/src/main/java/com/vaderetrosecure/keystore/dao/sql/KeyEntryType.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.vaderetrosecure.keystore.dao.sql;
+
+/**
+ * @author ahonore
+ *
+ */
+enum KeyEntryType
+{
+    PRIVATE_KEY,
+    SECRET_KEY
+}
---
+++ ssl-provider-keystore-sql/src/main/java/com/vaderetrosecure/keystore/dao/sql/StructureManager.java
@@ -0,0 +1,180 @@
+/**
+ * 
+ */
+package com.vaderetrosecure.keystore.dao.sql;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.apache.log4j.Logger;
+
+import com.vaderetrosecure.keystore.dao.KeyStoreDAOException;
+
+/**
+ * @author ahonore
+ *
+ */
+class StructureManager
+{
+    private static final Logger LOG = Logger.getLogger(StructureManager.class);
+
+    public static final String VERSIONS_TABLE = "versions";
+    public static final String KEYS_TABLE = "keys";
+    public static final String INTEGRITY_TABLE = "integrity";
+    public static final String NAMES_TABLE = "names";
+    public static final String CERTIFICATES_TABLE = "certificates";
+    public static final String PROTECTIONS_TABLE = "protections";
+    
+    private DataSource dataSource;
+    
+    public StructureManager(DataSource dataSource)
+    {
+        this.dataSource = dataSource;
+    }
+    
+    public boolean versionsTableExists() throws KeyStoreDAOException
+    {
+        try (Connection conn = dataSource.getConnection())
+        {
+            DatabaseMetaData meta = conn.getMetaData();
+            try (ResultSet rs = meta.getTables(null, null, VERSIONS_TABLE, new String[] {"TABLE"}))
+            {
+                if (!rs.next())
+                    return false;
+                
+                return true;
+            }
+        }
+        catch (SQLException e)
+        {
+            LOG.debug(e, e);
+            LOG.error(e);
+            throw new KeyStoreDAOException(e);
+        }
+    }
+    
+    public void manageKeysTable() throws KeyStoreDAOException
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.append("create table if not exists ");
+        sb.append(KEYS_TABLE);
+        sb.append(" (");
+        sb.append("alias_hash varchar(64) not null");sb.append(",");
+        sb.append("alias varchar(256) not null");sb.append(",");
+        sb.append("creation_date bigint default 0");sb.append(",");
+        sb.append("key_entry_type int default 0");sb.append(",");
+        sb.append("algorithm varchar(64)");sb.append(",");
+        sb.append("data text not null");sb.append(",");
+        sb.append("protection_iv varchar(128)");sb.append(",");
+        sb.append("protection_key text");sb.append(",");
+        sb.append("primary key(alias_hash)");
+        sb.append(")");
+
+        try (Connection conn = dataSource.getConnection())
+        {
+            try (PreparedStatement ps = conn.prepareStatement(sb.toString()))
+            {
+                ps.execute();
+            }
+            
+            insertVersion(conn, new Version(KEYS_TABLE, 1));
+        }
+        catch (SQLException e)
+        {
+            LOG.debug(e, e);
+            LOG.error(e);
+            throw new KeyStoreDAOException(e);
+        }
+    }
+    
+    public void manageCertificatesTable() throws KeyStoreDAOException
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.append("create table if not exists ");
+        sb.append(CERTIFICATES_TABLE);
+        sb.append(" (");
+        sb.append("alias_hash varchar(64) not null");sb.append(",");
+        sb.append("rank int default 0");sb.append(",");
+        sb.append("alias varchar(256) not null");sb.append(",");
+        sb.append("creation_date bigint default 0");sb.append(",");
+        sb.append("algorithm varchar(64)");sb.append(",");
+        sb.append("data text not null");sb.append(",");
+        sb.append("primary key(alias_hash, rank)");sb.append(",");
+        sb.append("key(algorithm)");
+        sb.append(")");
+
+        try (Connection conn = dataSource.getConnection())
+        {
+            try (PreparedStatement ps = conn.prepareStatement(sb.toString()))
+            {
+                ps.execute();
+            }
+            
+            insertVersion(conn, new Version(CERTIFICATES_TABLE, 1));
+        }
+        catch (SQLException e)
+        {
+            LOG.debug(e, e);
+            LOG.error(e);
+            throw new KeyStoreDAOException(e);
+        }
+    }
+    
+    public void manageIntegrityTable() throws KeyStoreDAOException
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.append("create table if not exists ");
+        sb.append(INTEGRITY_TABLE);
+        sb.append(" (");
+        sb.append("id bigint not null");sb.append(",");
+        sb.append("salt varchar(128) not null");sb.append(",");
+        sb.append("iv varchar(128) not null");sb.append(",");
+        sb.append("data varchar(256) not null");sb.append(",");
+        sb.append("data_hash varchar(64) not null");sb.append(",");
+        sb.append("primary key(id)");
+        sb.append(")");
+
+        try (Connection conn = dataSource.getConnection())
+        {
+            try (PreparedStatement ps = conn.prepareStatement(sb.toString()))
+            {
+                ps.execute();
+            }
+            
+            insertVersion(conn, new Version(INTEGRITY_TABLE, 1));
+        }
+        catch (SQLException e)
+        {
+            LOG.debug(e, e);
+            LOG.error(e);
+            throw new KeyStoreDAOException(e);
+        }
+    }
+    
+    public void createVersionsTable() throws KeyStoreDAOException
+    {
+        try (Connection conn = dataSource.getConnection(); PreparedStatement ps = conn.prepareStatement("create table if not exists " + VERSIONS_TABLE + " (table_name varchar(128) not null, version int not null, primary key(table_name))"))
+        {
+            ps.execute();
+        }
+        catch (SQLException e)
+        {
+            LOG.debug(e, e);
+            LOG.error(e);
+            throw new KeyStoreDAOException(e);
+        }
+    }
+    
+    private void insertVersion(Connection conn, Version version) throws SQLException
+    {
+        try (PreparedStatement ps = conn.prepareStatement("insert into " + VERSIONS_TABLE + " (table_name, version) value(?,?) on duplicate key update version=?"))
+        {
+            ps.executeUpdate();
+        }
+    }
+}
