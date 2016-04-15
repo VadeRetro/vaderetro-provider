ad18c8c56b36cbda890446b3716cf44b7bccda92
ahonore <antoine.honore@vade-retro.com>
2016-04-15 10:58:38 +0200

    doc

--- ssl-provider/src/main/java/com/vaderetrosecure/keystore/dao/IntegrityData.java
+++ ssl-provider/src/main/java/com/vaderetrosecure/keystore/dao/IntegrityData.java
@@ -20,6 +20,18 @@ import javax.crypto.SecretKey;
 import org.apache.log4j.Logger;
 
 /**
+ * This object represents integrity data that can be eventually checked.
+ * The check is done using a password:<br>
+ * <ul>
+ * <li>an AES-256 key is created, using the {@code salt}</li>
+ * <li>the {@code cipheredData} is deciphered, using the key and the {@code iv}</li>
+ * <li>the data is compared to {@code dataHash}</li>
+ * </ul>
+ * <br>
+ * The salt is used by all key protection objects to generate keys. Constructors are given to 
+ * easily generate new integrity data.<br>
+ * Each field is an array of bytes to be easily stored.
+ * 
  * @author ahonore
  *
  */
@@ -32,11 +44,22 @@ public class IntegrityData
     private byte[] cipheredData;
     private byte[] dataHash;
     
+    /**
+     * Construct a new IntegrityData object with generated fields.
+     */
     public IntegrityData()
     {
         this(CryptoTools.generateRandomBytes(16), CryptoTools.generateIV(), new byte[]{}, new byte[]{});
     }
 
+    /**
+     * Construct a new IntegrityData object given fields.
+     * 
+     * @param salt the salt used to generated keys using passwords.
+     * @param iv the initialization vector of the AES cipherer.
+     * @param cipheredData data to be evaluated on an integrity check.
+     * @param dataHash SHA-256 hash of the data to be compared with data.
+     */
     public IntegrityData(byte[] salt, byte[] iv, byte[] cipheredData, byte[] dataHash)
     {
         setSalt(salt);
@@ -46,18 +69,18 @@ public class IntegrityData
     }
 
     /**
-     * Generate a new integrity ciphering.
-     * Can be used change the password without loosing salt data.
+     * Generate a new IntegrityData object, given new salt and password fields.
+     * Can be used to change the password without loosing salt data.
      * 
-     * @param salt
-     * @param password
-     * @throws InvalidKeyException
-     * @throws NoSuchAlgorithmException
-     * @throws InvalidKeySpecException
-     * @throws NoSuchPaddingException
-     * @throws InvalidAlgorithmParameterException
-     * @throws IllegalBlockSizeException
-     * @throws BadPaddingException
+     * @param salt the salt used to generated keys using passwords.
+     * @param password the password used to generate new integrity data value.
+     * @throws InvalidKeyException if the generated key is wrong.
+     * @throws NoSuchAlgorithmException if used algorithms can not be found.
+     * @throws InvalidKeySpecException if keys can not be created.
+     * @throws NoSuchPaddingException if ciphering can not be performed.
+     * @throws InvalidAlgorithmParameterException if the ciphering algorithm can not used with this {@code iv}.
+     * @throws IllegalBlockSizeException if the ciphering can not be performed.
+     * @throws BadPaddingException if the ciphering can not be performed.
      */
     public IntegrityData(byte[] salt, char[] password) throws InvalidKeyException, NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException
     {
@@ -66,16 +89,17 @@ public class IntegrityData
     }
 
     /**
-     * Generate a completely new integrity data object.
+     * Generate a new IntegrityData object, given a new password.
+     * The salt will be generated.
      * 
-     * @param password
-     * @throws InvalidKeyException
-     * @throws NoSuchAlgorithmException
-     * @throws InvalidKeySpecException
-     * @throws NoSuchPaddingException
-     * @throws InvalidAlgorithmParameterException
-     * @throws IllegalBlockSizeException
-     * @throws BadPaddingException
+     * @param password the password used to generate new integrity data value.
+     * @throws InvalidKeyException if the generated key is wrong.
+     * @throws NoSuchAlgorithmException if used algorithms can not be found.
+     * @throws InvalidKeySpecException if keys can not be created.
+     * @throws NoSuchPaddingException if ciphering can not be performed.
+     * @throws InvalidAlgorithmParameterException if the ciphering algorithm can not used with this {@code iv}.
+     * @throws IllegalBlockSizeException if the ciphering can not be performed.
+     * @throws BadPaddingException if the ciphering can not be performed.
      */
     public IntegrityData(char[] password) throws InvalidKeyException, NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException
     {
@@ -83,41 +107,84 @@ public class IntegrityData
         generateIntegrity(password);
     }
 
+    /**
+     * Return the salt for generated password keys.
+     * 
+     * @return the salt as an array of bytes.
+     */
     public byte[] getSalt()
     {
         return salt;
     }
 
+    /**
+     * Define the salt for generated password keys.
+     * @param salt the salt as an array of bytes.
+     */
     public void setSalt(byte[] salt)
     {
         this.salt = salt;
     }
 
+    /**
+     * Retrieve the initialization vector to cipher/decipher data.
+     * 
+     * @return the initialization vector as an array of bytes.
+     */
     public byte[] getIV()
     {
         return iv;
     }
 
+    /**
+     * Set the initialization vector for ciphering/deciphering data.
+     * 
+     * @param iv the initialization vector.
+     */
     public void setIV(byte[] iv)
     {
         this.iv = iv;
     }
 
+    /**
+     * Return the ciphered data, processed when a check is performed.
+     * 
+     * @return the ciphered data.
+     */
     public byte[] getCipheredData()
     {
         return cipheredData;
     }
 
+    /**
+     * Define the ciphered data.
+     * This field is generated, so using this method with arrays not generated 
+     * by an other integrity data is strongly discouraged.
+     * 
+     * @param cipheredData the ciphered data.
+     */
     public void setCipheredData(byte[] cipheredData)
     {
         this.cipheredData = cipheredData;
     }
 
+    /**
+     * Return the hash of the data from the {@code cipheredData} field.
+     * 
+     * @return the hash of the data.
+     */
     public byte[] getDataHash()
     {
         return dataHash;
     }
 
+    /**
+     * Define the hash of the data from the {@code cipheredData} field.
+     * This field is generated, so using this method with arrays not generated 
+     * by an other integrity data is strongly discouraged.
+     * 
+     * @param dataHash the hash of the data.
+     */
     public void setDataHash(byte[] dataHash)
     {
         this.dataHash = dataHash;
@@ -133,6 +200,17 @@ public class IntegrityData
         setCipheredData(CryptoTools.cipherData(integrityData, secret, getIV()));
     }
     
+    /**
+     * Check the integrity, given a password.
+     * If the check succeeded, the method returns normally. Otherwise, an
+     * exception will be thrown.
+     * 
+     * @param password the password to perform the integrity check.
+     * @throws UnrecoverableKeyException if the check failed because of a wrong password.
+     * @throws IOException if fields can not be processed.
+     * @throws NoSuchAlgorithmException if the ciphering algorithm is unkown.
+     * @throws InvalidKeySpecException if the {@code iv} field is malformed.
+     */
     public void checkIntegrity(char[] password) throws UnrecoverableKeyException, IOException, NoSuchAlgorithmException, InvalidKeySpecException
     {
         try
