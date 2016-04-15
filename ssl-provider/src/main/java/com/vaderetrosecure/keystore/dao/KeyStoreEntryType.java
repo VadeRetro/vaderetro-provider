48bb24e265ee7b23cd795924cd6ce08d3a679d51
ahonore <antoine.honore@vade-retro.com>
2016-04-15 11:00:10 +0200

    doc

--- ssl-provider/src/main/java/com/vaderetrosecure/keystore/dao/KeyStoreEntryType.java
+++ ssl-provider/src/main/java/com/vaderetrosecure/keystore/dao/KeyStoreEntryType.java
@@ -4,12 +4,25 @@
 package com.vaderetrosecure.keystore.dao;
 
 /**
+ * The type of {@link com.vaderetrosecure.keystore.dao.KeyStoreEntry} object that is actually stored.
+ * 
  * @author ahonore
- *
  */
 public enum KeyStoreEntryType
 {
+    /**
+     * The entry is a secret key, i.e. a symmetric algorithm key.
+     */
     SECRET_KEY,
+    
+    /**
+     * The entry is a private key, .i.e. an asymmetric algorithm private key.
+     * A certificate chain may be attached to this entry.
+     */
     PRIVATE_KEY,
+    
+    /**
+     * A certificate, used as an authority for others.
+     */
     TRUSTED_CERTIFICATE
 }
