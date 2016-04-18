1bf31770fe8889156aedba4c24bf2bc25c2d218f
ahonore <antoine.honore@vade-retro.com>
2016-04-18 18:22:30 +0200

    doc

--- ssl-provider-keystore-sql/src/main/java/com/vaderetrosecure/keystore/dao/sql/EncodingTools.java
+++ ssl-provider-keystore-sql/src/main/java/com/vaderetrosecure/keystore/dao/sql/EncodingTools.java
@@ -15,8 +15,11 @@ import javax.xml.bind.DatatypeConverter;
 import org.apache.log4j.Logger;
 
 /**
+ * This class provides helper methods to encode/decode data to or from the store.
+ * It is useful for storing arrays of bytes from {@code KeyStoreEntry} objects as strings in the database. 
+ * 
  * @author ahonore
- *
+ * @see com.vaderetrosecure.keystore.dao.KeyStoreEntry
  */
 final class EncodingTools
 {
@@ -41,6 +44,12 @@ final class EncodingTools
     {
     }
     
+    /**
+     * Encode an array of bytes into a Base64 string.
+     * 
+     * @param data the array of bytes to encode.
+     * @return a Base64 encoded string. 
+     */
     public static String b64Encode(byte[] data)
     {
         if (data == null)
@@ -49,6 +58,12 @@ final class EncodingTools
         return new String(Base64.getEncoder().encode(data), StandardCharsets.US_ASCII);
     }
     
+    /**
+     * Decode a Base64 encoded string into an array of bytes. 
+     * 
+     * @param data the Base64 encoded string.
+     * @return an array of bytes.
+     */
     public static byte[] b64Decode(String data)
     {
         if (data == null)
@@ -57,6 +72,12 @@ final class EncodingTools
         return Base64.getDecoder().decode(data.getBytes(StandardCharsets.US_ASCII));
     }
     
+    /**
+     * Encode an array of bytes as a hexadecimal string.
+     * 
+     * @param data the array of bytes to encode.
+     * @return a hexadecimal encoded string. 
+     */
     public static String hexStringEncode(byte[] data)
     {
         if (data == null)
@@ -65,6 +86,12 @@ final class EncodingTools
         return DatatypeConverter.printHexBinary(data).toLowerCase();
     }
     
+    /**
+     * Decode a hexadecimal encoded string into an array of bytes. 
+     * 
+     * @param data the hexadecimal encoded string.
+     * @return an array of bytes.
+     */
     public static byte[] hexStringDecode(String data)
     {
         if (data == null)
@@ -73,6 +100,13 @@ final class EncodingTools
         return DatatypeConverter.parseHexBinary(data);
     }
     
+    /**
+     * Get the SHA-256 string from a string.
+     * The hash string is encoded in hexadecimal.
+     * 
+     * @param data the string to hash.
+     * @return the SHA-256 hash as a hexadecimal string.
+     */
     public static String toSHA2(String data)
     {
         LOCK.lock();
