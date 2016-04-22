cc7e7732965e22f86e1e91baa0ae6594e59053b8
ahonore <antoine.honore@vade-retro.com>
2016-04-22 15:09:05 +0200

    Replace all tab characters in this file by sequences of white-spaces

--- provider-core/src/main/java/com/vaderetrosecure/ssl/VRKeyManagerFactorySpi.java
+++ provider-core/src/main/java/com/vaderetrosecure/ssl/VRKeyManagerFactorySpi.java
@@ -54,19 +54,19 @@ public class VRKeyManagerFactorySpi extends KeyManagerFactorySpi
 
     private final static String VR_KEYSTORE_PRIVATE_KEY_FILE = "com.vaderetrosecure.key.private";
 
-	private KeyManager keyManagers[];
-	
-	public VRKeyManagerFactorySpi()
-	{
-	    keyManagers = null;
-	}
+    private KeyManager keyManagers[];
+    
+    public VRKeyManagerFactorySpi()
+    {
+        keyManagers = null;
+    }
 
     @Override
     protected KeyManager[] engineGetKeyManagers()
     {
-    	if (keyManagers == null)
-    		throw new IllegalStateException();
-    	
+        if (keyManagers == null)
+            throw new IllegalStateException();
+        
         return keyManagers;
     }
 
@@ -81,49 +81,49 @@ public class VRKeyManagerFactorySpi extends KeyManagerFactorySpi
     @Override
     protected void engineInit(ManagerFactoryParameters spec) throws InvalidAlgorithmParameterException
     {
-    	throw new UnsupportedOperationException();
+        throw new UnsupportedOperationException();
     }
 
     @Override
     protected void engineInit(KeyStore ks, char[] password) throws KeyStoreException, NoSuchAlgorithmException, UnrecoverableKeyException
     {
-    	if ((ks == null) || (!ks.getProvider().getName().equals(VadeRetroProvider.VR_PROVIDER)))
-    	{
-    	    KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
-    	    kmf.init(ks, password);
-    	    keyManagers = kmf.getKeyManagers();
-    	}
-    	
-		try
-		{
-			KeyStoreDAO ksdao = KeyStoreDAOFactory.getInstance().getKeyStoreDAO();
-			
-//			if (publicKey == null)
-//			    publicKey = loadPublicKeyProtection();
-			
-			IntegrityData integrityData = ksdao.getIntegrityData();
-			if (password != null)
-			    integrityData.checkIntegrity(password);
-	    	keyManagers = new KeyManager[] { new SNIX509ExtendedKeyManager(ksdao, loadKeyProtectionPrivateKey()) };
-		}
-		catch (KeyStoreDAOException e)
-		{
-			LOG.debug(e, e);
-			LOG.fatal(e);
-			throw new KeyStoreException(e);
-		}
-		catch (InvalidKeySpecException e)
-		{
-			LOG.debug(e, e);
-			LOG.fatal(e);
-			throw new UnrecoverableKeyException();
-		}
-		catch (IOException e)
-		{
-			LOG.debug(e, e);
-			LOG.fatal(e);
-			throw new KeyStoreException(e);
-		}
+        if ((ks == null) || (!ks.getProvider().getName().equals(VadeRetroProvider.VR_PROVIDER)))
+        {
+            KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+            kmf.init(ks, password);
+            keyManagers = kmf.getKeyManagers();
+        }
+        
+        try
+        {
+            KeyStoreDAO ksdao = KeyStoreDAOFactory.getInstance().getKeyStoreDAO();
+            
+//            if (publicKey == null)
+//                publicKey = loadPublicKeyProtection();
+            
+            IntegrityData integrityData = ksdao.getIntegrityData();
+            if (password != null)
+                integrityData.checkIntegrity(password);
+            keyManagers = new KeyManager[] { new SNIX509ExtendedKeyManager(ksdao, loadKeyProtectionPrivateKey()) };
+        }
+        catch (KeyStoreDAOException e)
+        {
+            LOG.debug(e, e);
+            LOG.fatal(e);
+            throw new KeyStoreException(e);
+        }
+        catch (InvalidKeySpecException e)
+        {
+            LOG.debug(e, e);
+            LOG.fatal(e);
+            throw new UnrecoverableKeyException();
+        }
+        catch (IOException e)
+        {
+            LOG.debug(e, e);
+            LOG.fatal(e);
+            throw new KeyStoreException(e);
+        }
     }
     
     private PrivateKey loadKeyProtectionPrivateKey()
